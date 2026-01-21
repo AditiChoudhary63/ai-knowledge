553ac2c6caf552e0865838600e2f993bf47e5b6c
AditiChoudhary63 <aditichoudhary2000ac@gmail.com>
2026-01-22 00:27:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from pathlib import Path
+from datetime import datetime
+import uuid
+
+
+
+print("Hello World")
+# @app.get("/health")
+# def health():
+#     return {"status": "ok"}
+
+
